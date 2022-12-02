54cd5c94f4a664cd24f326c7e20b7b58c78d1585
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2022-12-02 06:48:48 -0500

    completed homepage

--- tests/playwright.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('src/');
-  await page.waitForSelector('header');
-  await expect(page.locator('header')).toContainText('home');
-})
