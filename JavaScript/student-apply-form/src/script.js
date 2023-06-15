037c14266eea927291e927d42f3547e9474075d2
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-06-15 07:00:27 -0400

    setup form completed

---
+++ JavaScript/student-apply-form/src/script.js
@@ -0,0 +1,35 @@
+var selectedRow = null;
+
+//Show Alerts
+function showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+}
+
+//Clear all fields
+function clearFields() {
+    document.querySelector("#firstName").value = "";
+    document.querySelector("#lastName").value = "";
+    document.querySelector("#studentNumber").value = "";
+}
+
+//Add Data
+document.querySelector("#student-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+//Delete Data
+document.querySelector("#student-list").addEventListener("click", (e) => {
+    target = e.target;
+    if(target.classList.contains("delete")) {
+        target.parentElement.parentElement.remove();
+        showAlert("Student Data Deleted", "danger");
+    }
+})
