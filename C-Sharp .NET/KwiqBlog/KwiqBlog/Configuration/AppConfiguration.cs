e5516a68ce98fc54c844ec073b074d9847aaaf35
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-06-27 19:44:29 -0400

    rendering the views

---
+++ C-Sharp .NET/KwiqBlog/KwiqBlog/Configuration/AppConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace KwiqBlog.Configuration
+{
+    public static class AppConfiguration
+    {
+        public static void AddDefaultConfiguration(this IApplicationBuilder appBuilder, IWebHostEnvironment webHostEnv)
+        {
+            if (webHostEnv.IsDevelopment())
+            {
+                appBuilder.UseDeveloperExceptionPage();
+                appBuilder.UseDatabaseErrorPage();
+            } else
+            {
+                appBuilder.UseExceptionHandler("/Home/Error");
+                appBuilder.UseHsts();
+            }
+            appBuilder.UseHttpsRedirection();
+            appBuilder.UseStaticFiles();
+
+            appBuilder.UseRouting();
+
+            appBuilder.UseAuthentication();
+            appBuilder.UseAuthorization();
+
+            appBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ C-Sharp .NET/KwiqBlog/KwiqBlog/Configuration/AppServices.cs
@@ -0,0 +1,23 @@
+﻿using KwiqBlog.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KwiqBlog.Configuration
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConn")));
+            serviceCollection.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            serviceCollection.AddControllersWithViews();
+            serviceCollection.AddRazorPages();
+        }
+    }
+}
