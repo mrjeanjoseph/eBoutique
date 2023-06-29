fd2aed909465602a82a4d5bda511e2be3831d6c2
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-06-29 19:56:58 -0400

    started the admin page

---
+++ C-Sharp .NET/KwiqBlog/KwiqBlog/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KwiqBlog.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
