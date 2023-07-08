1522a373291ba987dd9a79a32b84ec2f49af7d83
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-07-08 16:20:29 -0400

    restructuring the proj

--- C-Sharp .NET/StudentCourses/StudentCourses.Web2/App_Start/BundleConfig.cs
+++ C-Sharp .NET/StudentCourses/StudentCourses.Web2/App_Start/BundleConfig.cs
@@ -11,13 +11,11 @@ namespace StudentCourses.Web2 {
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
