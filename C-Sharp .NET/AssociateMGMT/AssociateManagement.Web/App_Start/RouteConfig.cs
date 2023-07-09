f98de35d0f7522997e6fe1c1775d0141591ee804
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-07-09 06:31:48 -0400

    Made change to layout section

--- C-Sharp .NET/AssociateMGMT/AssociateManagement.Web/App_Start/RouteConfig.cs
+++ C-Sharp .NET/AssociateMGMT/AssociateManagement.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AssociateManagement.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Associate", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ C-Sharp .NET/AssociateMGMT/AssociateManagement.Web/Controllers/AssociateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssociateManagement.Web.Controllers
+{
+    public class AssociateController : Controller
+    {
+        // GET: Associate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
