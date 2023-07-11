9aa0cf6b347c962fa345395950f2391f283f3756
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-07-11 06:54:43 -0400

    Controller implementaion return 500 code error Bug06

--- C#.NET Apps/YouTubeProjects/EmployeeRecord/Controllers/HomeController.cs
+++ C#.NET Apps/YouTubeProjects/EmployeeRecord/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EmployeeRecord.Models;
 using System.Web.Mvc;
 
 namespace EmployeeRecord.Controllers {
     public class HomeController : Controller {
+
+        DEFAULTDB _empObject;
+
         public ActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(EmployeeModel emp) {
+            _empObject = new DEFAULTDB();
+
+            EmployeeRecord2 empData = new EmployeeRecord2 {
+                FirstName = emp.FirstName,
+                LastName = emp.LastName,
+                PhoneNumber = emp.PhoneNumber,
+                DateOfBirth = emp.DateOfBirth,
+                Address = emp.Address,
+                DepartmentId = emp.DepartmentId
+            };
+
+            _empObject.EmployeeRecords.Add(empData);
+            _empObject.SaveChanges();
+
+            return Json(new {SuccessMessage = "Data Added Successfully."}, JsonRequestBehavior.AllowGet);
+
         }
 
         public ActionResult About() {
---
+++ C#.NET Apps/YouTubeProjects/EmployeeRecord/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeRecord.Models {
+    public class EmployeeModel {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
