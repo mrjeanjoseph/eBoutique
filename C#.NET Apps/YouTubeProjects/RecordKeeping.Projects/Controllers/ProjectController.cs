6e22aab57e10d53932dd50c51701ae9c62d97799
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-07-12 07:17:38 -0400

    setting the the tables

--- C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Controllers/ProjectController.cs
+++ C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Controllers/ProjectController.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using RecordKeeping.Projects.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Linq;
 
-namespace RecordKeeping.Projects.Controllers
-{
+namespace RecordKeeping.Projects.Controllers {
     public class ProjectController : Controller
     {
+        AdventureWorksEntities _dptObj;
+        
         // GET: Project
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetDepartmentDetails() {
+            _dptObj = new AdventureWorksEntities();
+
+            List<DepartmentModel> deptList = 
+                (from obj in _dptObj.Departments
+                select new DepartmentModel {
+                    Name = obj.Name,
+                    GroupName = obj.GroupName,
+                    ModifiedDate = obj.ModifiedDate,
+                }).ToList();
+
+            return Json(deptList, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Models/DepartmentModel.cs
+++ C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Models/DepartmentModel.cs
@@ -5,5 +5,9 @@ using System.Web;
 
 namespace RecordKeeping.Projects.Models {
     public class DepartmentModel {
+        public short DepartmentID { get; set; }
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+        public System.DateTime ModifiedDate { get; set; } 
     }
 }
