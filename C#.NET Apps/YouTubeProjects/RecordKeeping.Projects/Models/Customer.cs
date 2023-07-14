509be3b9bbd0e8a189d0ca87dc23df1978ac9302
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-07-14 19:50:39 -0400

    removing additional codes

--- C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Models/Customer.cs
+++ C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Models/Customer.cs
@@ -1,27 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 
 namespace RecordKeeping.Projects.Models {
     public class Customer {
-        [Key] public int CustomerID { get; set; }
-
-        [Required(ErrorMessage = "Enter Name")]
-        public string Name { get; set; }
-
-        [Required(ErrorMessage = "Enter Address")]
-        public string Address { get; set; }
-
-        [Required(ErrorMessage = "Enter Mobileno")]
-        public string Mobileno { get; set; }
-
-        [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Enter Birthdate")]
-        public DateTime Birthdate { get; set; }
-
-        [Required(ErrorMessage = "Enter EmailID")]
-        public string EmailID { get; set; }
-
-        public List<Customer> ShowallCustomer { get; set; }
     }
 }
