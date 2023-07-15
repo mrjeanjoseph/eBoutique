11cc95a18ffede9bdd614e974dbb978c0b25e163
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-07-14 20:01:00 -0400

    view access rendered

---
+++ C#.NET Apps/YouTubeProjects/RecordKeeping.Projects/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using RecordKeeping.Projects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecordKeeping.Projects.Controllers {
+    public class CustomerController : Controller {
+
+        public ActionResult Index() {
+            return View();
+        }
+        // GET: /Customer/
+        [HttpGet] public ActionResult InsertCustomer() {
+
+            return View();
+        }
+
+        [HttpPost] public ActionResult InsertCustomer(Customer objCustomer) {
+
+            return View();
+        }
+    }
+}
