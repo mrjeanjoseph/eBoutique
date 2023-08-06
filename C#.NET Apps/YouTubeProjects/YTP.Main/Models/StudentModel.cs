f2a23d4f20ccef4d13656f8880e25b732fc40daa
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-06 09:51:56 -0400

    Updated the logic for the handler

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Models/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YTP.Main.Models {
+    public class StudentModel {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+    }
+}
