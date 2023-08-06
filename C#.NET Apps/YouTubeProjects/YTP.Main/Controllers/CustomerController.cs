29d35e6e677e7aa9df19a9192d3cdd3d1923df5c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-06 07:19:55 -0400

    Added customer detail logic

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/CustomerController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/CustomerController.cs
@@ -3,15 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YTP.Main.DataAccess;
+using YTP.Main.Models;
 
-namespace YTP.Main.Controllers
-{
-    public class CustomerController : Controller
-    {
+namespace YTP.Main.Controllers {
+
+    public class CustomerController : Controller {
+
         // GET: Customer
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
+
             return View();
+        }
+
+        // GET: /Customer/
+        [HttpGet]
+        public ActionResult InsertCustomer() {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult InsertCustomer(CustomerModel objCustomer) {
+
+            objCustomer.Birthdate = Convert.ToDateTime(objCustomer.Birthdate);
+
+            if (ModelState.IsValid){ //checking model is valid or not
+
+                DataAccessLayer objDB = new DataAccessLayer();
+                string result = objDB.InsertData(objCustomer);
+                ViewData["result"] = result;
+                ModelState.Clear(); //clearing model
+                return View();
+
+            } else {
+
+                ModelState.AddModelError("", "Error in saving data");
+                return View();
+
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ShowAllCustomerDetails() {
+
+            CustomerModel objCustomer = new CustomerModel();
+            DataAccessLayer objDB = new DataAccessLayer(); //calling class DBdata
+            objCustomer.ShowallCustomer = objDB.Selectalldata();
+
+            return View(objCustomer);
+
         }
     }
 }
