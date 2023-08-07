da143ff4c8a80bd68da2023543a0f9a1319ec726
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-07 06:06:49 -0400

    fixed an issue with null ref

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.Mvc;
+using YTP.Main.Models;
+
+namespace YTP.Main.Controllers {
+    public class EmployeeController : Controller
+    {
+
+        db dbop = new db();
+        string msg;
+
+        public ActionResult Index() {
+            Employee emp = new Employee();
+            emp.flag = "get";
+            DataSet ds = dbop.Empget(emp, out msg);
+            List<Employee> list = new List<Employee>();
+            foreach (DataRow dr in ds.Tables[0].Rows) {
+                list.Add(new Employee {
+                    Sr_no = Convert.ToInt32(dr["Sr_no"]),
+                    Emp_name = dr["Emp_name"].ToString(),
+                    City = dr["City"].ToString(),
+                    State = dr["State"].ToString(),
+                    Country = dr["Country"].ToString(),
+                    Department = dr["Department"].ToString()
+                });
+            }
+
+            return View(list);
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind] Employee emp) {
+
+            try {
+                emp.flag = "insert";
+                dbop.Empdml(emp, out msg);
+                TempData["msg"] = msg;
+
+            } catch (Exception ex) {
+                TempData["msg"] = ex.Message;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
