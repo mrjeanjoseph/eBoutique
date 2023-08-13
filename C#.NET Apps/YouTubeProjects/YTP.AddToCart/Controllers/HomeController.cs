4bfc9eb541dd10faef9df694d5a100b598e3a0b8
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-13 11:40:21 -0400

    view list added but not framed

--- C#.NET Apps/YouTubeProjects/YTP.AddToCart/Controllers/HomeController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.AddToCart/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using YTP.AddToCart.Models;
 using YTP.AddToCart.ViewModels;
@@ -56,6 +55,29 @@ namespace YTP.AddToCart.Controllers {
             }
 
             return Json(Session["VMCart"], JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ViewCart() {
+
+            List<VMCart> cartList = (List<VMCart>)Session["VMCart"];
+
+            List<VMViewCart> viewCartList = new List<VMViewCart>();
+
+            foreach(var item in cartList) {
+                VMViewCart cartItem = new VMViewCart();
+                tbl_product product = dbaccess.tbl_product.Where(p => p.id == item.pid).FirstOrDefault();
+
+                cartItem.ProductId = product.id;
+                cartItem.ProductName = product.PName;
+                cartItem.Qty = item.qty;
+                cartItem.ProductImg = product.PImage;
+                cartItem.UnitPrice = Convert.ToInt32(product.Price);
+                cartItem.TotalPrice = Convert.ToString(item.qty * Convert.ToDouble(product.Price));
+
+                viewCartList.Add(cartItem);
+            }
+
+            return View(viewCartList);
         }
 
         public ActionResult About() {
---
+++ C#.NET Apps/YouTubeProjects/YTP.AddToCart/ViewModels/VMViewCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YTP.AddToCart.ViewModels {
+    public class VMViewCart {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImg { get; set; }
+        public int Qty { get; set; }
+        public int UnitPrice { get; set; }
+        public string TotalPrice { get; set; }
+    }
+}
