f4ffb5cf90e573993be70e60288baed7b006238c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-20 08:25:24 -0400

    starting crud op with datatables

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/TBS_EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YTP.Main.Controllers {
+    public class TBS_EmployeeController : Controller {
+        // GET: TBS_Employee
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
