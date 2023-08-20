a0ccf43823385f32b7b2189001aa7c177e03dc1a
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-20 18:20:19 -0400

    Update operation completed

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/TBS_EmployeeController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/TBS_EmployeeController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using YTP.Main.Models;
 
@@ -40,10 +39,21 @@ namespace YTP.Main.Controllers {
         public ActionResult AddOrEdit(TBS_Employee empObj) {
 
             using (DBContext dbAccess = new DBContext()) {
-                dbAccess.tbs_Employees.Add(empObj);
-                dbAccess.SaveChanges();
 
-                return Json(new { success = true, message = "Data entry saved successfully." }, JsonRequestBehavior.AllowGet);
+                if (empObj.EmployeeId == 0) { // If it is 0, it must be adding new obj
+
+                    dbAccess.tbs_Employees.Add(empObj);
+                    dbAccess.SaveChanges();
+                    return Json(new { success = true, message = "Data entry saved successfully." }, JsonRequestBehavior.AllowGet);
+
+                } 
+                else {
+
+                    dbAccess.Entry(empObj).State = EntityState.Modified;
+                    dbAccess.SaveChanges();
+                    return Json(new { success = true, message = "Data modified and updated successfully." }, JsonRequestBehavior.AllowGet);
+
+                }
             }
         }
     }
