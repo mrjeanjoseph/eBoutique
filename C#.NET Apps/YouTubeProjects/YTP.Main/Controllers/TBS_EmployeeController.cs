da1ea45386e10ff6c86c85a95f72c653140f2b1e
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-21 04:45:16 -0400

    entire CRUD logic completed

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/TBS_EmployeeController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/TBS_EmployeeController.cs
@@ -8,53 +8,61 @@ namespace YTP.Main.Controllers {
     public class TBS_EmployeeController : Controller {
         // GET: TBS_Employee
 
+        private readonly DBContext dbAccess = new DBContext();
+
         public ActionResult Index() {
+
             return View();
         }
 
         public ActionResult GetData() {
 
-            using (DBContext dbAccess = new DBContext()) {
+            List<TBS_Employee> empList = dbAccess.tbs_Employees.ToList<TBS_Employee>();
+            return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
 
-                List<TBS_Employee> empList = dbAccess.tbs_Employees.ToList<TBS_Employee>();
-                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
-            }
         }
 
         [HttpGet]
         public ActionResult AddOrEdit(int id = 0) {
 
-            if(id == 0)
+            if (id == 0)
                 return View(new TBS_Employee());
             else {
-                using (DBContext dbAccess=new DBContext()) {
-                    var result = dbAccess.tbs_Employees.Where(e => e.EmployeeId == id).FirstOrDefault<TBS_Employee>();
 
-                    return View(result);
-                }
+                var result = dbAccess.tbs_Employees.Where(e => e.EmployeeId == id).FirstOrDefault<TBS_Employee>();
+                return View(result);
+
             }
         }
 
         [HttpPost]
         public ActionResult AddOrEdit(TBS_Employee empObj) {
 
-            using (DBContext dbAccess = new DBContext()) {
+            if (empObj.EmployeeId == 0) { // If it is 0, it must be adding new obj
 
-                if (empObj.EmployeeId == 0) { // If it is 0, it must be adding new obj
+                dbAccess.tbs_Employees.Add(empObj);
+                dbAccess.SaveChanges();
+                return Json(new { success = true, message = "Data entry saved successfully." }, JsonRequestBehavior.AllowGet);
 
-                    dbAccess.tbs_Employees.Add(empObj);
-                    dbAccess.SaveChanges();
-                    return Json(new { success = true, message = "Data entry saved successfully." }, JsonRequestBehavior.AllowGet);
+            } else {
 
-                } 
-                else {
+                dbAccess.Entry(empObj).State = EntityState.Modified;
+                dbAccess.SaveChanges();
+                return Json(new { success = true, message = "Data modified and updated successfully." }, JsonRequestBehavior.AllowGet);
 
-                    dbAccess.Entry(empObj).State = EntityState.Modified;
-                    dbAccess.SaveChanges();
-                    return Json(new { success = true, message = "Data modified and updated successfully." }, JsonRequestBehavior.AllowGet);
-
-                }
             }
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(int id) {
+
+            TBS_Employee empOjb = dbAccess.tbs_Employees.Where(e => e.EmployeeId == id).FirstOrDefault<TBS_Employee>();
+            dbAccess.tbs_Employees.Remove(empOjb);
+            dbAccess.SaveChanges();
+
+            return Json(new { success = true, message = "Data Deleted successfully." }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
