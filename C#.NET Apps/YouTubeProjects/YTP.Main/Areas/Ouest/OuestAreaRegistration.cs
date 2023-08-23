68108ac856a577ccb17b21350931439a539c35a4
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-23 19:56:25 -0400

    added area section of the project

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/Ouest/OuestAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YTP.Main.Areas.Ouest
+{
+    public class OuestAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Ouest";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Ouest_default",
+                "Ouest/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
