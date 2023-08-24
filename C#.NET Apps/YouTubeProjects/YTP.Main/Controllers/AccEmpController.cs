92e834b40e0c946b1e6edb4d006d9cc737dd8521
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-24 16:24:04 -0400

    added notify js file to accomodate dropdown notice

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/AccEmpController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/AccEmpController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 using YTP.Main.Models;
 using YTP.Main.ViewModel;
 
@@ -7,6 +8,7 @@ namespace YTP.Main.Controllers {
     public class AccEmpController : Controller {
 
         private readonly DBContext _dbContext;
+        string message = "";
         public AccEmpController() {
             _dbContext = new DBContext();
         }
@@ -17,12 +19,11 @@ namespace YTP.Main.Controllers {
                               select new SelectListItem() {
                                   Text = city.CityName, Value = city.CityId.ToString()
                               }).ToList();
-
             return View();
         }
 
         public JsonResult GetAllEmployee() {
-
+            message = "All data loaded successfully.";
             var accEmpRecord = (
                 from empData in _dbContext.Acc_EmpData
                 join cityData in _dbContext.Acc_CityData on empData.CityId equals cityData.CityId
@@ -37,18 +38,18 @@ namespace YTP.Main.Controllers {
                     cityData.CityName
                 }).ToList();
 
-            return Json(new {Success = true, data = accEmpRecord }, JsonRequestBehavior.AllowGet);
+            return Json(new { Success = true, data = accEmpRecord }, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult AddUpdateEmployee(AccEmpData viewModel) {
 
-            string message = "Data successfully Updated";
-            if(!ModelState.IsValid) {
+            message = "Data successfully Updated";
+            if (!ModelState.IsValid) {
                 var errorList = (from item in ModelState
                                  where item.Value.Errors.Any()
                                  select item.Value.Errors[0].ErrorMessage).ToList();
 
-                return Json(new {Success = false, Message = "An error occurred! We think we know why.",  ErrorList = errorList});
+                return Json(new { Success = false, Message = "An error occurred! We think we know why.", ErrorList = errorList });
             }
 
             Acc_EmpData empObj = _dbContext.Acc_EmpData
@@ -61,13 +62,35 @@ namespace YTP.Main.Controllers {
             empObj.Salary = viewModel.Salary;
             empObj.CityId = viewModel.CityId;
 
-            if(viewModel.EmployeeId == 0) {
+            if (viewModel.EmployeeId == 0) {
                 message = "Data successfully added";
                 _dbContext.Acc_EmpData.Add(empObj);
             }
 
             _dbContext.SaveChanges();
-            return Json(new { Success = true, Message = message }, JsonRequestBehavior.AllowGet);
+            return Json(new { Success = true, message }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult EditEmployee(int employeeId) {
+
+            return Json(_dbContext.Acc_EmpData.SingleOrDefault(model => model.EmployeeId == employeeId),
+                JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEmployee(int employeeId) {
+
+            Acc_EmpData accEmp = _dbContext.Acc_EmpData.Single(model => model.EmployeeId == employeeId);
+
+            message = $"{accEmp.FirstName} has been deleted successfully";
+
+
+            _dbContext.Acc_EmpData.Remove(accEmp);
+            _dbContext.SaveChanges();
+            return Json(new {
+                Success = true,
+                message
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
