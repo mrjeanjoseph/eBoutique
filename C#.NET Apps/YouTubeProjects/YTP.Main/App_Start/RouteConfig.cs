7daeba527bbc2e822d51f43ef25c51feb10958a1
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-29 05:12:18 -0400

    Updated the app config to fix an error

--- C#.NET Apps/YouTubeProjects/YTP.Main/App_Start/RouteConfig.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/App_Start/RouteConfig.cs
@@ -2,8 +2,11 @@
 using System.Web.Routing;
 
 namespace YTP.Main {
+
     public class RouteConfig {
+
         public static void RegisterRoutes(RouteCollection routes) {
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
@@ -11,12 +14,6 @@ namespace YTP.Main {
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 
-            );
-            routes.MapRoute(
-                name: "ShoppingCart",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional },
-                namespaces: new[] { "YTP.Main.Areas.ShoppingCart.Controllers" }
             );
 
         }
