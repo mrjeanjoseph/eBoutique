1cae100a27d23e501fc0900fbf5b4e786ea1ef37
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-08-31 04:47:05 -0400

    cart section added

--- C#.NET Apps/YouTubeProjects/YTP.Main/Areas/ShoppingCart/Controllers/ShoppingController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/ShoppingCart/Controllers/ShoppingController.cs
@@ -9,10 +9,12 @@ namespace YTP.Main.Areas.ShoppingCart.Controllers {
     public class ShoppingController : Controller {
 
         private readonly DBContext _cartdbcontext;
+        private List<VM_Cart> _listcartitemcontext;
 
         public ShoppingController() {
 
             _cartdbcontext = new DBContext();
+            _listcartitemcontext = new List<VM_Cart>();
         }
 
         public ActionResult Index() {
@@ -35,6 +37,43 @@ namespace YTP.Main.Areas.ShoppingCart.Controllers {
                     }).ToList();
                             
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public JsonResult Index(string ItemId) {
+
+            VM_Cart cartmodel = new VM_Cart();
+            Item item = _cartdbcontext.Items.Single(model => model.ItemId.ToString() == ItemId);
+
+            if (Session["CartCounter"] != null) {
+                _listcartitemcontext = Session["cartitem"] as List<VM_Cart>;
+            }
+
+            if(_listcartitemcontext.Any(model => model.ItemId == ItemId)) {
+                cartmodel = _listcartitemcontext.Single(model => model.ItemId == ItemId);
+                cartmodel.Quantity++;
+                cartmodel.Total = cartmodel.Quantity * cartmodel.UnitPrice;
+            }
+            else { 
+                cartmodel.ItemId = ItemId;
+                cartmodel.ItemName = item.ItemName;
+                cartmodel.ImagePath = item.ImagePath;
+                cartmodel.Quantity = 1;
+                cartmodel.Total = item.ItemPrice;
+                cartmodel.UnitPrice = item.ItemPrice;
+
+                _listcartitemcontext.Add(cartmodel);
+            }
+
+            Session["CartCounter"] = _listcartitemcontext.Count;
+            Session["CartItem"] = _listcartitemcontext;
+
+            return Json(new {
+                Success = true, 
+                Counter = _listcartitemcontext.Count, 
+                Message = "Item added to cart successfully"
+            }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/ShoppingCart/Models/VM_Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YTP.Main.Areas.ShoppingCart.Models {
+    public class VM_Cart {
+        public string ItemId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+        public string ImagePath { get; set; }
+        public string ItemName { get; set; }
+    }
+}
