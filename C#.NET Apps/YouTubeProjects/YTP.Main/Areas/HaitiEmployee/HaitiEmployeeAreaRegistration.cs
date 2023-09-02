ffd0e4c28d52695ff9d245c30180acdd593d7e92
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-09-02 07:23:11 -0400

    starting the Haiti Record project

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/HaitiEmployee/HaitiEmployeeAreaRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace YTP.Main.Areas.HaitiEmployee
+{
+    public class HaitiEmployeeAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get{return "HaitiEmployee";}
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HaitiEmployee_default",
+                "HaitiEmployee/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
