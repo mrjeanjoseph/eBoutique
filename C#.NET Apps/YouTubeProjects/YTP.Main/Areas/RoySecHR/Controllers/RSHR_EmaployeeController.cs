3e2adac2ff4473988a3f221810a47d25bc441c5f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-09-03 19:11:03 -0400

    processing data access and logic input

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/RoySecHR/Controllers/RSHR_EmaployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YTP.Main.Areas.RoySecHR.Controllers
+{
+    public class RSHR_EmaployeeController : Controller
+    {
+        // GET: RoySecHR/RSHR_Emaployee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
