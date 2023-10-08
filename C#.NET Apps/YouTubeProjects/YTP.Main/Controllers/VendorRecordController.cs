9f670a378d47927aac42c3f1479a11de39264b68
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-08 10:02:14 -0400

    api made it to main project

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/VendorRecordController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/VendorRecordController.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using YTP.Main.Models;
 
 namespace YTP.Main.Controllers {
     public class VendorRecordController : Controller {
         // GET: VendorRecord
         public ActionResult Index() {
-            return View();
+
+            IEnumerable<VendorRecordModel> vendorRec;
+
+            HttpResponseMessage response = GlobalVariables.webApiClient.GetAsync("VendorRecord").Result;
+            vendorRec = response.Content.ReadAsAsync<IEnumerable<VendorRecordModel>>().Result;
+
+            return View(vendorRec);
         }
     }
 }
