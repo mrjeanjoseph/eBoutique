8042f31da5ec13a150e8f145ab58e32010c84c92
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-08 17:36:06 -0400

    implementing edit functionality

--- C#.NET Apps/YouTubeProjects/YTP.Main/App_Start/BundleConfig.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/App_Start/BundleConfig.cs
@@ -15,25 +15,25 @@ namespace YTP.Main {
                       "~/GlobalAssets/styles/main.site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                      "~/GlobalAssets/ScriptFiles/jquery-{version}.js"));
+                      "~/GlobalAssets/scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                      "~/GlobalAssets/ScriptFiles/jquery.validate*"));
+                      "~/GlobalAssets/scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
-                      "~/GlobalAssets/ScriptFiles/jquery-ui-1.13.2.min.js"));
+                      "~/GlobalAssets/scripts/jquery-ui-1.13.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/DataTables").Include(
-                      "~/GlobalAssets/ScriptFiles/jquery.dataTables.min.js",
-                      "~/GlobalAssets/ScriptFiles/dataTables.bootstrap5.min.js"));
+                      "~/GlobalAssets/scripts/jquery.dataTables.min.js",
+                      "~/GlobalAssets/scripts/dataTables.bootstrap5.min.js"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/GlobalAssets/ScriptFiles/bootstrap.bundle.js"));
+                      "~/GlobalAssets/scripts/bootstrap.bundle.js"));
 
             bundles.Add(new Bundle("~/bundles/globalscripts").Include(
-                      "~/GlobalAssets/ScriptFiles/notify.min.js",
-                      "~/GlobalAssets/ScriptFiles/alertify.min.js",
-                      "~/GlobalAssets/ScriptFiles/main.site.js"));
+                      "~/GlobalAssets/scripts/notify.min.js",
+                      "~/GlobalAssets/scripts/alertify.min.js",
+                      "~/GlobalAssets/scripts/main.site.js"));
             
             //All Local Styles and Scripts
             bundles.Add(new Bundle("~/bundles/localscripts").Include(
