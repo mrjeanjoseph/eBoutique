1b313965def252cb6b3a3470a4fde4d6702bbc29
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-09 18:38:11 -0400

    Added validation and metadata

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/VehicleRentalSystem/Models/CustomerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using static YTP.Main.Areas.VehicleRentalSystem.Models.VehicleModel;
+
+namespace YTP.Main.Areas.VehicleRentalSystem.Models {
+
+    [MetadataType(typeof(CustomerMetaData))]
+
+    public partial class Customer {
+
+        public class CustomerMetaData {
+
+            [DisplayName("Customer Name")] public string CustomerName { get; set; }
+            [DisplayName("Address")] public string CompleteAddress { get; set; }
+            [DisplayName("Phone Number")] public Nullable<int> PhoneNumber { get; set; }
+        }
+
+    }
+}
