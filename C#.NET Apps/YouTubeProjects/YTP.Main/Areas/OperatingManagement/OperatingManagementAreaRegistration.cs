8f416e19560644e1693f92d62ae204c59b753aed
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-09 05:11:32 -0400

    added operating mgmt area for upcoming proj

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/OperatingManagement/OperatingManagementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YTP.Main.Areas.OperatingManagement
+{
+    public class OperatingManagementAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "OperatingManagement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "OperatingManagement_default",
+                "OperatingManagement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
