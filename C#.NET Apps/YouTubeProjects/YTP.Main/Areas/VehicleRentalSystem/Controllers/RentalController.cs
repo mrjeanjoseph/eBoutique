318c32a650bb7c8e4a199b02ee120df191740dbd
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-10 06:48:38 -0400

    loaded all the vehicles in the dropdown

--- C#.NET Apps/YouTubeProjects/YTP.Main/Areas/VehicleRentalSystem/Controllers/RentalController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/VehicleRentalSystem/Controllers/RentalController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YTP.Main.DataAccess;
 
 namespace YTP.Main.Areas.VehicleRentalSystem.Controllers {
     public class RentalController : Controller {
+
+        private readonly DBContext _db = new DBContext();
         // GET: VehicleRentalSystem/Rental
         public ActionResult Index() {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetVehicles() {
+            var getVehicles = _db.Vehicles.ToList();
+
+            return Json(getVehicles, JsonRequestBehavior.AllowGet);
         }
 
 
