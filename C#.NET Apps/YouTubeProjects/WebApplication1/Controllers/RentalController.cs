62de42d92add58961e66801d36415990da90be3a
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-14 09:51:20 -0400

    both cars and customers ajax running

--- C#.NET Apps/YouTubeProjects/WebApplication1/Controllers/RentalController.cs
+++ C#.NET Apps/YouTubeProjects/WebApplication1/Controllers/RentalController.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
-namespace WebApplication1.Controllers
-{
-    public class RentalController : Controller
-    {
+namespace WebApplication1.Controllers {
+
+    public class RentalController : Controller {
+
+        readonly supercarEntities _db = new supercarEntities();
+
         // GET: Rental
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
+
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetCar() {
+
+            return Json(_db.carregs.ToList(), JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public ActionResult Getid(int id) {
+
+            var customer = (from c in _db.customers where c.id == id select c.custname).ToList();
+            return Json(customer, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public ActionResult CheckAvailability(string carno) {
+
+            var availability = (from c in _db.carregs where c.carno == carno select c.available).FirstOrDefault();
+            return Json(availability, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
