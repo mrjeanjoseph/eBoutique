156a28538b3e407d763a49bcdbd710510ea2892a
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-10-14 07:11:38 -0400

    addressed a validation issue with customer model

--- C#.NET Apps/YouTubeProjects/WebApplication1/Models/customervalidation.cs
+++ C#.NET Apps/YouTubeProjects/WebApplication1/Models/customervalidation.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models {
-    public partial class customer {
 
+    [MetadataType(typeof(customerMetaData))]
+    public partial class customer {
+        public class customerMetaData {
+
+
+            [DisplayName("Customer Name")]
+            public string custname { get; set; }
+
+            [DisplayName("Physical Address")]
+            public string address { get; set; }
+
+            [DisplayName("Mobile Number")]
+            public int? mobile { get; set; }
+        }
     }
 }
