29965543cf146a42dee1c2ac8b57ecc01194dcaf
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-07 21:57:29 -0500

    pushing the limit and moving the in mem to vm

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/HomeController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/HomeController.cs
@@ -9,46 +9,14 @@ using YTP.Main.ViewModels;
 namespace YTP.Main.Controllers {
     public class HomeController : Controller {
         // GET: Home
-        public ActionResult Index() {
+        //private readonly VM_MiniDisplay _md = new VM_MiniDisplay();
 
-            var miniDisplay = new List<MiniDisplay>() {
-                new MiniDisplay {
-                    DisplayName = "This Year Pickup Truck", 
-                    DisplayDescription = "This is detailed description of mini display one.",
-                    DisplayImageLink = "https://images8.alphacoders.com/616/thumb-1920-616810.jpg"
-                },
-                new MiniDisplay {
-                    DisplayName = "Just Another TTP", 
-                    DisplayDescription = "This is detailed description of mini display Rep.",
-                    DisplayImageLink = "https://images.alphacoders.com/644/644418.jpg"
-                },
-                new MiniDisplay {
-                    DisplayName = "Mini Display - Rep", 
-                    DisplayDescription = "This is detailed description of mini display Rep.",
-                    DisplayImageLink = ""
-                },
-                new MiniDisplay {
-                    DisplayName = "Mini Display Three", 
-                    DisplayDescription = "This is detailed description of mini display Three."
-                },
-                new MiniDisplay {
-                    DisplayName = "One of a kind trucks", 
-                    DisplayDescription = "This is detailed description of cool looking truck.",
-                    DisplayImageLink = "https://wallpapers.com/images/hd/ford-truck-tr8rsjeai7pbxeny.jpg"
-                },
+        public ActionResult Index(VM_MiniDisplay viewModel) {
 
-            };
+            var result = viewModel.Something();
 
-            var module = new List<Module>() {
-                new Module {ModuleName = "CRUD Main Page", ModuleDescription = "This is just another detail module description"}
-            };
+            return View(result);
 
-            var miniDisplayObj = new VM_MiniDisplay {
-                MiniDisplay = miniDisplay,
-                Module = module,
-            };
-
-            return View(miniDisplayObj);
         }
 
         public ActionResult UnderConstruction() {
