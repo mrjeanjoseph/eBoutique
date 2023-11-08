ea6f4436e6b113d76df9c6c1b7e4cfa3f56b32e5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-07 20:48:51 -0500

    Looks like we got to work

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/HomeController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/HomeController.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YTP.Main.Models;
+using YTP.Main.ViewModels;
 
 namespace YTP.Main.Controllers {
     public class HomeController : Controller {
         // GET: Home
         public ActionResult Index() {
-            return View();
+
+            var Module = new List<Module>() {
+                new Module {ModuleName = "Module one", ModuleDescription = "This is detailed description of module one."},
+                new Module {ModuleName = "Module Two - Rep", ModuleDescription = "This is detailed description of module Rep."},
+                new Module {ModuleName = "Module Three", ModuleDescription = "This is detailed description of module Three."},
+                new Module {ModuleName = "Module ThreeB", ModuleDescription = "This is detailed description of module ThreeB."},
+            };
+
+            var Page = new List<Page>() {
+                new Page {PageName = "This is a page", PageDescription = "This is just another detail page description"}
+            };
+
+            var ModuleViewModel = new ModuleViewModel {
+                Modules = Module,
+                Pages = Page,
+            };
+
+            return View(ModuleViewModel);
         }
 
         public ActionResult UnderConstruction() {
---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/ViewModels/ModuleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using YTP.Main.Models;
+
+namespace YTP.Main.ViewModels {
+    public class ModuleViewModel {
+        public List<Module> Modules { get; set; }
+        public List<Page> Pages { get; set; }
+    }
+}
