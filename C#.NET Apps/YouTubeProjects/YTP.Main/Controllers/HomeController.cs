9c2385346c6fee61d8646fedb2ec35cafe2850f6
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-08 20:08:24 -0500

    follow along to this tutorial

--- C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/HomeController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Controllers/HomeController.cs
@@ -14,9 +14,25 @@ namespace YTP.Main.Controllers {
         public ActionResult Index(VM_MiniDisplay viewModel) {
 
             var result = viewModel.GenerateMiniDisplay();
+            int hour = DateTime.Now.Hour;
+            string message;
+
+            if (hour < 12) message = "Good morning";
+            else if (hour > 12 && hour < 16) message = "Good Afternoon";
+            else message = "Good Evening";
+
+            ViewBag.Greeting = message;
 
             return View(result);
 
+        }
+
+        public ActionResult SomeStylings() {
+
+            string cssStyling = "background-color: yellow;";
+            ViewBag.cssStyling = cssStyling;
+
+            return View("index");
         }
 
         public ActionResult UnderConstruction() {
---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Models/RSVPForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YTP.Main.Models {
+    public class RSVPForm {
+        public string RSVP_Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool WillAttend { get; set; }
+
+    }
+}
