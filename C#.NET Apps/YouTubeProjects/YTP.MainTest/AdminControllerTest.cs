f4713e7f90f0b3bfefe417b9a8919cb6857c37da
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-09 20:19:48 -0500

    added a reference to the assembly the newly created method

--- C#.NET Apps/YouTubeProjects/YTP.MainTest/AdminControllerTest.cs
+++ C#.NET Apps/YouTubeProjects/YTP.MainTest/AdminControllerTest.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using YTP.Main.Controllers;
 using YTP.Main.DataAccess;
 using YTP.Main.Models;
 
@@ -9,23 +12,37 @@ namespace YTP.MainTest {
         [TestMethod]
         public void CanChangeLoginName() {
             //Arrange (Set up a scenario)
-            Member member = new Member() { LoginName = "Louna" };
+            Member member = new Member() { LoginName = "lounapaniague" };
             FakeMembersRepository repoParam = new FakeMembersRepository();
-            repoParam.member
+            repoParam.Members.Add(member);
+            AdminController target = new AdminController(repoParam);
+            string oldLoginname = member.LoginName;
+            string newLoginName = "lounajeanjoseph";
+
+            //Act (Attempt the operation)
+            target.ChangeLoginName(oldLoginname, newLoginName);
+
+            //Assert (Verify the result)
+            Assert.AreEqual(newLoginName, member.LoginName);
+            Assert.IsTrue(repoParam.DidSubmitChanges);
         }
     }
 
     public class FakeMembersRepository : IMembersRepository {
+
+        public List<Member> Members = new List<Member>();
+        public bool DidSubmitChanges = false;
+
         public void AddMember(Member member) {
             throw new NotImplementedException();
         }
 
         public Member FetchByLoginName(string loginName) {
-            throw new NotImplementedException();
+            return Members.First(m => m.LoginName == loginName);
         }
 
         public void SubmitChanges() {
-            throw new NotImplementedException();
+            DidSubmitChanges = true;
         }
     }
 }
