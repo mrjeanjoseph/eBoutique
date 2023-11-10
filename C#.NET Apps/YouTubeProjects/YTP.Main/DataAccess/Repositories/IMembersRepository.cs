8fe157fc9b32d96147ee34312794f0415803e5e0
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-09 19:51:09 -0500

    implementing private backend field.

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/DataAccess/Repositories/IMembersRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YTP.Main.Models;
+
+namespace YTP.Main.DataAccess {
+    public interface IMembersRepository {
+        void AddMember(Member member);
+        Member FetchByLoginName(string loginName);
+        void SubmitChanges();
+    }
+}
--- C#.NET Apps/YouTubeProjects/YTP.Main/DataAccess/Repositories/MembersRepository.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/DataAccess/Repositories/MembersRepository.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Web;
 using YTP.Main.Models;
 
-namespace YTP.Main.DataAccess.Repositories {
-    public class MembersRepository {
+namespace YTP.Main.DataAccess {
+    public class MembersRepository : IMembersRepository {
         public void AddMember(Member member) {
             // Implement moi
         }
 
-        public void FetchByLoginName(string login_name) {
+        public void FetchByLoginName(string loginName) {
             // Implement moi
         }
 
         public void SubmitChanges() {
             // Implement moi
+        }
+
+        Member IMembersRepository.FetchByLoginName(string loginName) {
+            throw new NotImplementedException();
         }
     }
 }
