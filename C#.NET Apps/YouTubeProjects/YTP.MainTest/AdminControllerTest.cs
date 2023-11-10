54698eafbf4c0aba4247d82dc98c877d600ceea6
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-09 20:05:22 -0500

    unit test feels like debugging

---
+++ C#.NET Apps/YouTubeProjects/YTP.MainTest/AdminControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YTP.Main.DataAccess;
+using YTP.Main.Models;
+
+namespace YTP.MainTest {
+    [TestClass]
+    public class AdminControllerTest {
+        [TestMethod]
+        public void CanChangeLoginName() {
+            //Arrange (Set up a scenario)
+            Member member = new Member() { LoginName = "Louna" };
+            FakeMembersRepository repoParam = new FakeMembersRepository();
+            repoParam.member
+        }
+    }
+
+    public class FakeMembersRepository : IMembersRepository {
+        public void AddMember(Member member) {
+            throw new NotImplementedException();
+        }
+
+        public Member FetchByLoginName(string loginName) {
+            throw new NotImplementedException();
+        }
+
+        public void SubmitChanges() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#.NET Apps/YouTubeProjects/YTP.MainTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YTP.MainTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YTP.MainTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ea40b34-dead-48e8-a200-5701c2ccfa62")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
