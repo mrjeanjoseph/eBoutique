f425c3ebd02ef5d94cf637b3a2ac3ef7220e3c52
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-10 20:19:58 -0500

    Creating a playground area for textbook projects.

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SandboxTBP/SandboxTBPAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YTP.Main.Areas.SandboxTBP
+{
+    public class SandboxTBPAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SandboxTBP";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SandboxTBP_default",
+                "SandboxTBP/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
