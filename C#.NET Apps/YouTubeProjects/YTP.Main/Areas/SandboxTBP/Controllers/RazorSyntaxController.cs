4a0cdc8f2103582cb52b97bfceccf38ae195fc34
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-11 20:22:03 -0500

    working with razor syntax

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SandboxTBP/Controllers/RazorSyntaxController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YTP.Main.Models;
+
+namespace YTP.Main.Areas.SandboxTBP.Controllers {
+    public class RazorSyntaxController : Controller {
+
+        Product RazorProduct = new Product() {
+            ProductID = 1,
+            Name = "Bouyon Pye Bef",
+            Description = "Bouyon as fet ak pye bef",
+            Category = "Kizin Nasyonal",
+            ProductPrice = 950M
+        };
+
+        // GET: SandboxTBP/RazorSyntax
+        public ActionResult Index() {
+            return View(RazorProduct);
+        }
+    }
+}
