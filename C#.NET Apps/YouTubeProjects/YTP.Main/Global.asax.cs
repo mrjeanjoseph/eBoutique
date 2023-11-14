9b089d7dde5fd6837b8344e213419e3b5f381b3f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-14 18:15:24 -0500

    cleaned up additional files

--- C#.NET Apps/YouTubeProjects/YTP.Main/Global.asax.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Global.asax.cs
@@ -2,22 +2,14 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using YTP.Main.Infrastructure;
 
 namespace YTP.Main {
     public class MvcApplication : HttpApplication {
         protected void Application_Start() {
-
             AreaRegistration.RegisterAllAreas();
-
-            DependencyResolver.SetResolver(new NinjectDependencyResolver());
-            //GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver();
-
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
         }
-
     }
 }
