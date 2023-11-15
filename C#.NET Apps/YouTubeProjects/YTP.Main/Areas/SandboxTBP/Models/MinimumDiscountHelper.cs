2a5ccc4f3eb5a32951512dbb5e389685f5ad99d7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-15 18:27:20 -0500

    setting up unit tests

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SandboxTBP/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YTP.Main.Areas.SandboxTBP.Models {
+
+    public class MinimumDiscountHelper : IDiscountHelper {
+
+        public decimal ApplyDiscount(decimal totalParam) {
+
+            throw new NotImplementedException();
+        }
+    }
+}
