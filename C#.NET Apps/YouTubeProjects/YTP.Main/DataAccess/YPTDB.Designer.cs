2dd9ba72442b86bf8231e6cb9c7a700434c36bd1
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-16 05:22:47 -0500

    changes solution to reflect new development

--- C#.NET Apps/YouTubeProjects/YTP.Main/DataAccess/YPTDB.Designer.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/DataAccess/YPTDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\admin\source\_workspace\CRUD\C#.NET Apps\YouTubeProjects\YTP.Main\DataAccess\YPTDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\admin\source\_workspace\CRUD\C#.NET Apps\SandboxWebProjectEnv\YTP.Main\DataAccess\YPTDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
