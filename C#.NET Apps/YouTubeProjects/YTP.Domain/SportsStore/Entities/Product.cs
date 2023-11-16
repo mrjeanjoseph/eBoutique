cf0825c3cb2617a080380f442be92c44c148e793
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-16 06:41:09 -0500

    wiring up models to view

--- C#.NET Apps/YouTubeProjects/YTP.Domain/SportsStore/Entities/Product.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Domain/SportsStore/Entities/Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace YTP.Domain.SportsStore.Entities {
+﻿namespace YTP.Domain.SportsStore.Entities {
     public class Product {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
--- C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/SportsStoreAreaRegistration.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/SportsStoreAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace YTP.Main.Areas.SportsStore
             context.MapRoute(
                 "SportsStore",
                 "SportsStore/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { GetType().Namespace + ".Controllers" }
             );
         }
     }
