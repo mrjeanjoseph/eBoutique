133377984c3dffbfa97a3ecf8108bf8a51d851e7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-16 05:50:20 -0500

    abstract and mock repo

---
+++ C#.NET Apps/YouTubeProjects/YTP.Domain/SportsStore/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTP.Domain.SportsStore.Entities {
+    public class Product {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string Category { get; set; }
+        public int IsAvailable { get; set; }
+    }
+}
