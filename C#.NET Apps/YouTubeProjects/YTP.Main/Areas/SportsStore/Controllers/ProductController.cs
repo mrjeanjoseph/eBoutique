6fb25b6113dc0ad06c02cd6e189ce04cfc42b18c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-16 19:55:48 -0500

    added html helper method

--- C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/Controllers/ProductController.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/Controllers/ProductController.cs
@@ -1,19 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
 using YTP.Domain.SportsStore.Abstract;
 
 namespace YTP.Main.Areas.SportsStore.Controllers {
 
     public class ProductController : Controller {
         private readonly IProductsRepository _productRepo;
+        public int PageSize = 4;
 
         public ProductController(IProductsRepository productRepo) {
             _productRepo = productRepo;
         }
 
         // GET: SportsStore/Product
-        public ActionResult ListProducts() {
+        public ActionResult ListProducts(int page = 1) {
 
-            return View(_productRepo.Products);
+            return View(_productRepo.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize));
+            //return View(_productRepo.Products);
         }
     }
 }
--- C#.NET Apps/YouTubeProjects/YTP.MainTest/SportsStore/UnitTest1.cs
+++ C#.NET Apps/YouTubeProjects/YTP.MainTest/SportsStore/UnitTest1.cs
@@ -1,11 +1,40 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using YTP.Main.Areas.SportsStore.Controllers;
+using YTP.Domain.SportsStore.Abstract;
+using YTP.Domain.SportsStore.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
 
 namespace YTP.MainTest.SportsStore {
+
     [TestClass]
     public class UnitTest1 {
+
         [TestMethod]
-        public void TestMethod1() {
+        public void CanPaginate() {
+
+            //arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, ProductName = "P1"},
+                new Product {ProductID = 2, ProductName = "P2"},
+                new Product {ProductID = 3, ProductName = "P3"},
+                new Product {ProductID = 4, ProductName = "P4"},
+                new Product {ProductID = 5, ProductName = "P5"},
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.ListProducts(2); //
+
+            //Assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].ProductName, "P4");
+            Assert.AreEqual(prodArray[1].ProductName, "P5");
         }
     }
 }
