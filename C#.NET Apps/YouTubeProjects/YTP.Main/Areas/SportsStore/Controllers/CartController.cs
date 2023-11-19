40f6c57d9196fa7728763879c3755fa764875061
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-18 21:29:01 -0500

    AddToCart logic and session completed

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using YTP.Domain.SportsStore.Abstract;
+using YTP.Domain.SportsStore.Entities;
+using YTP.Main.Areas.SportsStore.Models;
+
+namespace YTP.Main.Areas.SportsStore.Controllers {
+    public class CartController : Controller {
+
+        private readonly IProductsRepository _repository;
+
+        public CartController(IProductsRepository repository) {
+            _repository = repository;
+        }
+
+        public ActionResult Index(string returnUrl) {
+            return View(new CartIndex_VM {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl) {
+            Product product = _repository.Products
+                    .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null) {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl) {
+            Product product = _repository.Products
+        .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null) {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart() {
+
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null) {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/Models/CartIndex_VM.cs
@@ -0,0 +1,8 @@
+﻿using YTP.Domain.SportsStore.Entities;
+
+namespace YTP.Main.Areas.SportsStore.Models {
+    public class CartIndex_VM {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
