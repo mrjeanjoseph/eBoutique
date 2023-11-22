27318e16b5195b2987aea37653d7ee55a8342a92
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-21 21:11:40 -0500

    Admin test passed and completed

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YTP.Domain.SportsStore.Abstract;
+
+namespace YTP.Main.Areas.SportsStore.Controllers {
+
+    public class AdminController : Controller {
+
+        private readonly IProductsRepository _productRepo;
+
+        public AdminController(IProductsRepository products) {            
+            _productRepo = products;
+        }
+
+        // GET: SportsStore/Admin
+        public ViewResult Index() {
+            return View(_productRepo.Products);
+        }
+    }
+}
