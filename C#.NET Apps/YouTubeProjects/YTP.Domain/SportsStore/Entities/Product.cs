ae0f153468dc3c2e206852953ee0a9cd1a5a8360
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-22 07:11:00 -0500

    annotating product model with metadata

--- C#.NET Apps/YouTubeProjects/YTP.Domain/SportsStore/Entities/Product.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Domain/SportsStore/Entities/Product.cs
@@ -1,12 +1,21 @@
-﻿namespace YTP.Domain.SportsStore.Entities {
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace YTP.Domain.SportsStore.Entities {
 
     public class Product {
 
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
         public string ProductName { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
         public decimal ProductPrice { get; set; }
         public string Category { get; set; }
         public int IsAvailable { get; set; }
+
     }
+
 }
