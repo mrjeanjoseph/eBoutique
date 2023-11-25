acfc06c7efd2b4cf68fc71797e318d36995eaa30
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-25 10:59:38 -0500

    setting up the box for urls and routing systems

---
+++ C#.NET Apps/YouTubeProjects/YTP.Main/Areas/UrlsAndRoutes/UrlsAndRoutesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YTP.Main.Areas.UrlsAndRoutes
+{
+    public class UrlsAndRoutesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "UrlsAndRoutes";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "UrlsAndRoutes",
+                "UrlsAndRoutes/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- C#.NET Apps/YouTubeProjects/YTP.MainTest/UrlsAndRoutes/UnitTest1.cs
+++ C#.NET Apps/YouTubeProjects/YTP.MainTest/UrlsAndRoutes/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.Web.Routing;
 using Moq;
 using System;
 using System.Web;
-using YTP.Main;
+using YTP.Main.Areas.UrlsAndRoutes;
 
 namespace YTP.MainTest.UrlsAndRoutes {
     [TestClass]
