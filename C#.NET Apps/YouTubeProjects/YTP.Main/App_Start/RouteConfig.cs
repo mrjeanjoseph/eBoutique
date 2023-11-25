168e934da335df5a2c20cf2ad237212aa37efa34
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2023-11-25 10:18:38 -0500

    reordering the routing system

--- C#.NET Apps/YouTubeProjects/YTP.Main/App_Start/RouteConfig.cs
+++ C#.NET Apps/YouTubeProjects/YTP.Main/App_Start/RouteConfig.cs
@@ -9,10 +9,13 @@ namespace YTP.Main {
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("MyRoute", "{controller}/{action}",
+                new { controller = "Home", action = "Index", });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional },
                 new string[] { "YTP.Main.Controllers" }
             );
         }
